f2cd2eed6ec521d9d40fe16ac639438a63022780
roy-276 <maingiroy95@gmail.com>
2023-03-22 10:40:31 +0300

    Home Page

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Home.css';
+import Product from './Product';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg"
+          alt=""
+          className="home__image"
+        />
+
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <Product />
+        <div className="home__row"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css';
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>Title</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>30</strong>
+        </p>
+        <div className="product__rating"> ⭐ ⭐ ⭐</div>
+      </div>
+
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+        alt=""
+      />
+
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
