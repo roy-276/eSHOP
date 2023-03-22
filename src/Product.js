7fa0e7ff885cb06cd59a8c679225a5374456c0a6
roy-276 <maingiroy95@gmail.com>
2023-03-22 11:17:30 +0300

    Props

--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({ id, title, price, rating, image }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>Title</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>30</strong>
+          <strong>{price}</strong>
         </p>
-        <div className="product__rating"> ⭐ ⭐ ⭐</div>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
       </div>
 
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
-        alt=""
-      />
+      <img src={image} alt="" />
 
       <button>Add to Basket</button>
     </div>
